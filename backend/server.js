1e1f7b5b186c8613c95799146e4d400d49b3ce28
maicol-monge <maicol.monge@catolica.edu.sv>
2026-05-03 14:47:56 -0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+require("dotenv").config(); //Cargar variables d entorno
+const express = require("express"); //Crear servidor web
+const cors = require("cors"); //Para permitir solicitudes desde otro dominio
+
+const db = require("./config/db");
+
+const app = express(); //Instancia del servidor
+ //Evitar errores al consumir en React
+const allowedOrigins = [
+  
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Permitir solicitudes sin origin (como curl o Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('No permitido por CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+app.use(express.json()); //Recibir los datos en JSON
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error conectando a la base de datos:", err);
+    process.exit(1); // Sale de la aplicación en caso de error
+  }
+  console.log("Conectado a la base de datos MySQL");
+});
+
+// Iniciar servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor backend corriendo en el puerto ${PORT}`);
+});
+
+// Rutas
